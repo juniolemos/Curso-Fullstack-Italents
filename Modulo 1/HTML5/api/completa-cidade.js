b52138ebfecf753ce2d7b5313fd5752e08795bc1
Junio Lemos <junio.lemos@hotmail.com>
2023-05-22 22:51:19 -0300

    recuperacao

--- Modulo 1/HTML5/api/completa-cidade.js
+++ Modulo 1/HTML5/api/completa-cidade.js
@@ -1,34 +1,34 @@
-$(".input-partida").autocomplete({
-    source: function(request, response) {
+$(document).ready(function() {
+    $("#input-partida, #input-saida").autocomplete({
+      source: function(request, response) {
         const apiUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios";
-
-
+        const cacheKey = request.term.toLowerCase();
+  
+        // Verificar se os dados estão armazenados em cache
+        const cachedData = localStorage.getItem(cacheKey);
+        if (cachedData) {
+          const cidadesFormatadas = JSON.parse(cachedData);
+          response(cidadesFormatadas);
+          return;
+        }
+  
         $.getJSON(apiUrl, { nome: request.term }, function(cidades) {
-
-            const cidadesFormatadas = cidades.map(function(cidade) {
-                return { label: cidade.nome, value: cidade.nome };
-            });
-
-
-            response(cidadesFormatadas);
+          const filtro = request.term.toLowerCase();
+          const cidadesFiltradas = cidades.filter(function(cidade) {
+            return cidade.nome.toLowerCase().startsWith(filtro);
+          });
+  
+          const cidadesFormatadas = cidadesFiltradas.map(function(cidade) {
+            return { label: cidade.nome, value: cidade.nome };
+          });
+  
+          // Armazenar os dados em cache
+          localStorage.setItem(cacheKey, JSON.stringify(cidadesFormatadas));
+  
+          response(cidadesFormatadas);
         });
-    },
-    minLength: 3
-});
-$(".input-saida").autocomplete({
-    source: function(request, response) {
-        const apiUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios";
-
-
-        $.getJSON(apiUrl, { nome: request.term }, function(cidades) {
-
-            const cidadesFormatadas = cidades.map(function(cidade) {
-                return { label: cidade.nome, value: cidade.nome };
-            });
-
-
-            response(cidadesFormatadas);
-        });
-    },
-    minLength: 3
-});
+      },
+      minLength: 3
+    });
+  });
+  
--- Modulo 1/HTML5/js/script-carrinho.js
+++ Modulo 1/HTML5/js/script-carrinho.js
@@ -10,3 +10,127 @@ for (let i = 0; i < excluir.length; i++) {
     });
 };
 // };
+///Retornando a home.html 
+$(document).ready(function() {
+    $("#voltar").click(function() {
+      window.location.href = "home.html";
+    });
+  });
+  
+//excluindo itens carrinho ida
+$(document).ready(function() {
+    $("#diminuir").click(function() {
+      var numeroProduto = parseInt($("#numero-produto").text());
+      var valorProduto = parseFloat($("#valor-produto").text());
+      var valorUnitario = valorProduto / numeroProduto;
+
+      
+      
+      if (!isNaN(numeroProduto) && numeroProduto > 0) {
+        $("#numero-produto").text(numeroProduto - 1);
+        $("#valor-produto").text((valorUnitario * (numeroProduto - 1)).toFixed(2));
+      }
+    });
+  });
+//excluindo itens carrinho volta
+  $(document).ready(function() {
+    $("#diminuir2").click(function() {
+      var numeroProduto2 = parseInt($("#numero-produto2").text());
+      var valorProduto2 = parseFloat($("#valor-produto2").text());
+      var valorUnitario2 = valorProduto2 / numeroProduto2;
+
+      
+      
+      if (!isNaN(numeroProduto2) && numeroProduto2 > 0) {
+        $("#numero-produto2").text(numeroProduto2 - 1);
+        $("#valor-produto2").text((valorUnitario2 * (numeroProduto2 - 1)).toFixed(2));
+      }
+    });
+  });
+
+  //adicionar itens
+  $(document).ready(function() {
+    $("#somar").click(function() {
+      var numeroProduto = parseInt($("#numero-produto").text());
+      var valorProduto = parseFloat($("#valor-produto").text());
+      var valorUnitario = valorProduto / numeroProduto;
+
+      
+      
+      if (!isNaN(numeroProduto) && numeroProduto > 0) {
+        $("#numero-produto").text(numeroProduto + 1);
+        $("#valor-produto").text((valorUnitario * (numeroProduto +1)).toFixed(2));
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    $("#somar2").click(function() {
+      var numeroProduto2 = parseInt($("#numero-produto2").text());
+      var valorProduto2 = parseFloat($("#valor-produto2").text());
+      var valorUnitario2 = valorProduto2 / numeroProduto2;
+
+      
+      
+      if (!isNaN(numeroProduto2) && numeroProduto2 > 0) {
+        $("#numero-produto2").text(numeroProduto2 + 1);
+        $("#valor-produto2").text((valorUnitario2 * (numeroProduto2 +1)).toFixed(2));
+      }
+    });
+  });
+  //somar total
+  $(document).ready(function() {
+    $("#somar2").click(function() {
+      var valorProduto = parseFloat($("#valor-produto").text().replace(",", "."));
+      var valorProduto2 = parseFloat($("#valor-produto2").text().replace(",", "."));
+    
+      var totalGeral = valorProduto + valorProduto2;
+  
+      $("#total").text(totalGeral.toFixed(2).replace(".", ","));
+    });
+  });
+  $(document).ready(function() {
+    $("#somar").click(function() {
+      var valorProduto = parseFloat($("#valor-produto").text().replace(",", "."));
+      var valorProduto2 = parseFloat($("#valor-produto2").text().replace(",", "."));
+    
+      var totalGeral = valorProduto + valorProduto2;
+  
+      $("#total").text(totalGeral.toFixed(2).replace(".", ","));
+    });
+  });
+//abater do total
+  $(document).ready(function() {
+    $("#diminuir").click(function() {
+      var valorProduto = parseFloat($("#valor-produto").text().replace(",", "."));
+      var valorProduto2 = parseFloat($("#valor-produto2").text().replace(",", "."));
+    
+      var totalGeral = valorProduto + valorProduto2;
+  
+      $("#total").text(totalGeral.toFixed(2).replace(".", ","));
+    });
+  });
+  $(document).ready(function() {
+    $("#diminuir2").click(function() {
+      var valorProduto = parseFloat($("#valor-produto").text().replace(",", "."));
+      var valorProduto2 = parseFloat($("#valor-produto2").text().replace(",", "."));
+    
+      var totalGeral = valorProduto + valorProduto2;
+  
+      $("#total").text(totalGeral.toFixed(2).replace(".", ","));
+    });
+  });
+
+ // Limpar carrinho
+ $(document).ready(function() {
+    $("#excluir-cart").click(function() {
+      $("#numero-produto2").text("0");
+      $("#numero-produto").text("0");
+      $("#valor-produto2").text("0,00");
+      $("#valor-produto").text("0,00");
+      $("#total").text("0,00");
+      
+      
+      alert("Seu carrinho esta vazio, escolha o seu destino!");
+    });
+  });
